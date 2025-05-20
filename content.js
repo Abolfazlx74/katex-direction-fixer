e20d5fe0edb2f3b781e5c72b24e6c41ec71c8609
Abolfazlx74 <ramz13851385@gmail.com>
2025-05-20 01:53:04 -0800

    Fix inline mathematics rendering by removing incorrect DOM manipulation

--- content.js
+++ content.js
@@ -1,17 +1,24 @@
 function fixDirection() {
+  // Elements that display mathematics in a block
   document.querySelectorAll('.katex-display').forEach(el => {
     el.style.direction = 'ltr';
   });
-  // document.querySelectorAll('.katex:not(.katex-display .katex) .katex-html').forEach(el => {
-  //   if (1) {
-  //     console.log(el.querySelectorAll('.mrel:not(.mrel.mtight)'))
-  //   }
-  // })
+
+  // Elements that display mathematics in an inline style   
+  document.querySelectorAll('.katex:not(.katex-display)').forEach(el => {
+    const parentP = el.closest('p');
+    if (parentP) {
+      parentP.style.display = "flex";
+      parentP.style.alignItems = "center";
+    }
+    el.style.direction = "ltr";
+    el.style.padding = "2px 4px";
+  });
 }
 
 const observer = new MutationObserver(() => {
   fixDirection();
 });
 
-observer.observe(document.body, { childList: true, subtree: true });
+observer.observe(document.body, {childList: true, subtree: true});
 fixDirection();
