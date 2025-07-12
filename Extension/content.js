936534add0f33694bfb313b68a1053dba888d5d5
Abolfazlx74 <ramz13851385@gmail.com>
2025-07-12 04:07:02 -0800

    Fix display for tables

--- Extension/content.js
+++ Extension/content.js
@@ -1,19 +1,23 @@
 function fixDirection() {
   // Elements that display mathematics in a block
   document.querySelectorAll('.katex-display').forEach(el => {
-    el.style.direction = 'ltr';
+    if(!el.closest('._tableContainer_80l1q_1')){
+      el.style.direction = 'ltr';
+    }
   });
 
   // Elements that display mathematics in an inline style   
   document.querySelectorAll('.katex:not(.katex-display)').forEach(el => {
-    const parentP = el.closest('p');
-    if (parentP) {
-      parentP.style.display = "flex";
-      parentP.style.flexWrap = "wrap";
-      if (isMultiline(parentP))parentP.style.alignItems = "center";
+    if(!el.closest('._tableContainer_80l1q_1')){
+      const parentP = el.closest('p');
+      if (parentP) {
+        parentP.style.display = "flex";
+        parentP.style.flexWrap = "wrap";
+        if (isMultiline(parentP))parentP.style.alignItems = "center";
+      }
+      el.style.direction = "ltr";
+      el.style.padding = "2px 4px";
     }
-    el.style.direction = "ltr";
-    el.style.padding = "2px 4px";
   });
 }
 
